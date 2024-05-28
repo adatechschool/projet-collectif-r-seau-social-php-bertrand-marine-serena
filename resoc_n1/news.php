c3a9868c8a60e597af532f349eba8d80e150f671
Bertrand MARIE <photomb09@gmail.com>
2024-05-28 10:09:39 +0200

    review news.php

--- resoc_n1/news.php
+++ resoc_n1/news.php
@@ -100,6 +100,7 @@
                     echo "<article>";
                     echo("Échec de la requete : " . $mysqli->error);
                     echo("<p>Indice: Vérifiez la requete  SQL suivante dans phpmyadmin<code>$laQuestionEnSql</code></p>");
+                    echo "</article>";
                     exit();
                 }
 
@@ -109,7 +110,7 @@
                 {
                     //la ligne ci-dessous doit etre supprimée mais regardez ce 
                     //qu'elle affiche avant pour comprendre comment sont organisées les information dans votre 
-                    // echo "<pre>" . print_r($post, 1) . "</pre>";
+                    //echo "<pre>" . print_r($post, 1) . "</pre>";
 
                     // @todo : Votre mission c'est de remplacer les AREMPLACER par les bonnes valeurs
                     // ci-dessous par les bonnes valeurs cachées dans la variable $post 
@@ -134,7 +135,6 @@
                     // avec le <?php ci-dessus on retourne en mode php 
                 }// cette accolade ferme et termine la boucle while ouverte avant.
                 ?>
-
             </main>
         </div>
     </body>
