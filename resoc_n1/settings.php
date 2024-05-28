6019ce79abcd4c4565b8f0146fa1303a93dc9082
Marine Delfosse <re-mamarane@hotmail.fr>
2024-05-28 11:57:49 +0200

    Modif settings.php

--- resoc_n1/settings.php
+++ resoc_n1/settings.php
@@ -58,13 +58,13 @@
                  */
                 $laQuestionEnSql = "
                     SELECT users.*, 
-                    count(DISTINCT posts.id) as totalpost, 
-                    count(DISTINCT given.post_id) as totalgiven, 
-                    count(DISTINCT recieved.user_id) as totalrecieved 
+                    COUNT(DISTINCT posts.id) AS totalpost, 
+                    COUNT(DISTINCT given.post_id) AS totalgiven, 
+                    COUNT(DISTINCT recieved.user_id) AS totalrecieved 
                     FROM users 
                     LEFT JOIN posts ON posts.user_id=users.id 
-                    LEFT JOIN likes as given ON given.user_id=users.id 
-                    LEFT JOIN likes as recieved ON recieved.post_id=posts.id 
+                    LEFT JOIN likes AS given ON given.user_id=users.id 
+                    LEFT JOIN likes AS recieved ON recieved.post_id=posts.id 
                     WHERE users.id = '$userId' 
                     GROUP BY users.id
                     ";
@@ -85,15 +85,15 @@
                     <h3>Mes paramètres</h3>
                     <dl>
                         <dt>Pseudo</dt>
-                        <dd>Félicie</dd>
+                        <dd><?php echo $user['alias']; ?></dd>
                         <dt>Email</dt>
-                        <dd>felicie@test.org</dd>
+                        <dd><?php echo $user['email']; ?></dd>
                         <dt>Nombre de message</dt>
-                        <dd>42</dd>
+                        <dd><?php echo $user['totalpost']; ?></dd>
                         <dt>Nombre de "J'aime" donnés </dt>
-                        <dd>12</dd>
+                        <dd><?php echo $user['totalgiven']; ?></dd>
                         <dt>Nombre de "J'aime" reçus</dt>
-                        <dd>53</dd>
+                        <dd><?php echo $user['totalrecieved']; ?></dd>
                     </dl>
 
                 </article>
